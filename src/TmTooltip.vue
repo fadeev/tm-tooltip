051eeb9ec0b1417e7c5fcee9a4aa7cbfd349bfb5
Denis Fadeev <denis@fadeev.org>
2019-09-05 16:16:26 +0500

    Hacky css reset

--- src/TmTooltip.vue
+++ src/TmTooltip.vue
@@ -8,6 +8,74 @@
 </template>
 
 <style scoped>
+/deep/ html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+button,
+input {
+  font-weight: inherit;
+  font-style: inherit;
+  font-family: inherit;
+  font-size: 100%;
+  vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+}
+
 /deep/ h1 {
   font-size: 0.875rem;
   font-weight: 500;
